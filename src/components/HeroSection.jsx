2c9733eae2a911d3da92fc533b822e5d3b2710bd
Darin Josell <darin.j.manley@gmail.com>
2025-09-28 19:38:38 -0400

    Fix Netlify deployment and image paths

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -55,7 +55,7 @@ function HeroSection() {
       <div className="home-e-11">
         <div className="home-e-14">
           <Text tag="span" className="home-e-15">
-            <Image src="assets/template-asset-5cc2b48f" alt="Image" />
+            <Image src="/assets/template-asset-5cc2b48f" alt="Image" />
           </Text>
           <Text tag="span" className="home-e-12">
             <Text tag="p">
@@ -83,7 +83,7 @@ function HeroSection() {
           </div>
         </div>
         <Text tag="span" className="home-e-19">
-          <Image src="assets/template-asset-711551c0" alt="Image" />
+          <Image src="/assets/template-asset-711551c0" alt="Image" />
         </Text>
       </div>
     </div>
