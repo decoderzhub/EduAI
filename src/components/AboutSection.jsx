33e040fd779a1acadfac49d8bd428a938192be52
Darin Josell <darin.j.manley@gmail.com>
2025-09-28 22:17:15 -0400

    Updated AboutSection.jsx

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -6,35 +6,29 @@ function AboutSection() {
   return (
     <div className="home2-e-28" data-cdhs="up">
         <div className="home2-e-29">
-            <div className="home2-e-41"> 
-                <Text tag="span" className="home2-e-40"> 
-                    <Image src="/assets/asset-5de5ac27.png" alt="About EduAI Bridge" /> 
-                </Text>
+            <div className="home2-e-41"> <Text tag="span" className="home2-e-40"> <Image src="https://assets-v2.codedesign.ai/storage/v1/object/public/68d9ba37be7e5676b63a52e3_d9e7f34a/asset-5de5ac27" alt="Image" /> </Text>
                 <div className="home2-e-42">
                     <div className="home2-e-43"> <i className="fas fa-book-open home2-e-44"></i></div>
                 </div>
             </div>
-            <div className="home2-e-30"> 
-                <Text tag="span" className="home2-e-31">
-                    <Text tag="p">TEMPORIBUS</Text>
+            <div className="home2-e-30"> <Text tag="span" className="home2-e-31">
+                    <Text tag="p">TEMPORIBUS</Text><Text tag="span" className="home2-e-32">
+                        <Text tag="p">Add text in text element</Text>
+                    </Text><Text tag="span" className="home2-e-33">
+                        <Text tag="p">Add text in text element</Text>
+                    </Text><Text tag="span" className="home2-e-34">
+                        <Text tag="p">Add text in text element</Text>
+                    </Text><Text tag="span" className="cd-paragraph-clean home2-e-35">
+                        <Text tag="h2">Header 2</Text>
+                    </Text>
                     <div className="home2-e-36"> </div>
-                </Text>
-                <Text tag="span" className="home2-e-37">
-                    <Text tag="p"><strong>Get to Know About Eginary Education</strong></Text>
-                </Text>
-                <Text tag="span" className="home2-e-39">
-                    <Text tag="p">Neque porro quisquam est, qui dolorem ipsum dolor sit amet, consectetur quis non numquam eius modi incidunt dolor.</Text>
-                </Text>
-                <Text tag="span" className="home2-e-39">
-                    <Text tag="p">Omnis dolor repellendus. Temporibus autem qui aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur.</Text>
-                </Text>
-                <Text tag="span" className="home2-e-39">
-                    <Text tag="p">Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere est omnis voluptas assumenda est, omnis dolor repellendus temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur.</Text>
-                </Text>
-                <Text tag="span" className="home2-e-39">
-                    <Text tag="p">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni.</Text>
-                </Text>
-            </div>
+                </Text><Text tag="span" className="home2-e-37">
+                    <Text tag="p"><strong>Advance Your Carrier Lear Something New</strong></Text>
+                </Text><Text tag="span" className="home2-e-39">
+                    <Text tag="p">Neque porro quisquam est, qui dolorem ipsum dolor sit amet, consectetursed quia non numuam eius modi incidunt dolor in reprehenderit in voluta dolore eu fugiat nulla pariatur.</Text>
+                </Text><button className="home2-e-38"><Link href="About" target="_self" className="link">
+                        <Text tag="p">Read more</Text>
+                    </Link></button></div>
         </div>
     </div>
   )
