b9fa41dcdd055b2c90c3d79e29229b1d1b132da4
Darin Josell <darin.j.manley@gmail.com>
2025-09-28 22:40:18 -0400

    Updated Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -42,7 +42,7 @@ function Home() {
                 <div className="home-e-7" data-clientid="home-e-7"> <input className="home-e-6" data-clientid="home-e-6" /></div><button className="home-e-8" data-clientid="home-e-8">
                     <Text tag="p">LETS TALK</Text>
                 </button>
-                            </Link></Text></div><<button className="About-e-122"><Link href="contact" target="_self" className="link">
+                            </Link></Text></div><button className="About-e-122"><Link href="contact" target="_self" className="link">
                             <Text tag="p">CONTACT US</Text>
                         </Link></button>
             </div>
